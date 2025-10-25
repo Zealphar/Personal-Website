6e58a73a0d91cd294ff63cf8beec1656a7c13c84
Jared Manuel <105923719+Zealphar@users.noreply.github.com>
2025-10-25 04:24:36 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var typed= new typed('.text', {
+    strings: ['Web Designer', 'Barista', 'Coach'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+var typed = new typed("#text", {
+    Strings: ["Web Designer", "Barista"],
+    typeSpeed: 300,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
